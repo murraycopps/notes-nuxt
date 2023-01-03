10133fe695dfcc45a4096031e98c72afd3992893
murraycopps <murraycopps@gmail.com>
2023-01-03 09:15:11 -0500

    styled notecards

--- components/NoteCard.vue
+++ components/NoteCard.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="card bg-yellow-300 flex flex-col shadow-lg rounded-md p-8 text-center">
-        <h1 :class="`text-2xl font-bold text-gray-800 p-2 rounded-xl mb-4`">{{ title }}
+    <div class="card">
+        <h1 class="title">{{ title }}
         </h1>
-        <p :class="`text-lg text-gray-700 grow p-2 rounded-xl`">{{ body }}</p>
+        <p class="`text`">{{ body }}</p>
     </div>
 </template>
     
@@ -26,6 +26,30 @@ export default {
 .card {
     width: clamp(300px, 50%, 500px);
     min-height: 300px;
+    background-color: rgb(253 224 71);
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    border-radius: 0.375rem;
+    padding: 2rem;
+    text-align: center;
+    margin: 1rem;
+}
+
+.title {
+    font-size: 1.5rem;
+    line-height: 2rem;
+    color: rgb(31 41 55);
+    padding: .5rem;
+    margin-bottom: 1rem;
+}
+
+.text {
+    color: rgb(55 65 81);
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+    padding: 0.5rem;
+    flex-grow: 1;
 }
 </style>
     
